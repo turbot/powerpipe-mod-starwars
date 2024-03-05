bfe99d4c28b3d61293409431ca98b3591be7a301
souravTurbot <78197905+bigdatasourav@users.noreply.github.com>
2024-03-05 21:25:13 +0530

    Update mod.pp

--- mod.pp
+++ mod.pp
@@ -1,6 +1,7 @@
 mod "starwars" {
   title         = "Star Wars"
   description   = "Analyze characters, species, planets, spaceships and vehicle performance data using DuckDb and Powerpipe."
+  color         = "#FFE81F"
   documentation = file("./README.md")
   categories    = ["dashboard", "duckdb"]
 }
