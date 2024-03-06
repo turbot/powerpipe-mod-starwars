1c469d90f1e1f34561698b67b135097a5fdbb6a1
sourav chakraborty <sourav@turbot.com>
2024-03-06 17:04:59 +0530

    update

--- mod.pp
+++ mod.pp
@@ -4,10 +4,10 @@ mod "starwars" {
   color         = "#FFE81F"
   documentation = file("./README.md")
   categories    = ["dashboard", "duckdb"]
-}
 
-opengraph {
-  title       = "Powerpipe Mod for Star Wars"
-  description = "Visualize Star Wars character, species, planet, spaceship data, and more using Powerpipe and DuckDB."
-  image       = "/images/mods/turbot/starwars-social-graphic.png"
+  opengraph {
+    title       = "Powerpipe Mod for Star Wars"
+    description = "Visualize Star Wars character, species, planet, spaceship data, and more using Powerpipe and DuckDB."
+    image       = "/images/mods/turbot/starwars-social-graphic.png"
+  }
 }
