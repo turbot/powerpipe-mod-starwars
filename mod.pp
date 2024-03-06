c0e656c64bf41f4a3072528b6607622b138c59ac
Ved misra <47312748+misraved@users.noreply.github.com>
2024-03-07 01:00:48 +0530

    Update mod.pp to use the correct path for mod documentation

--- mod.pp
+++ mod.pp
@@ -2,7 +2,7 @@ mod "starwars" {
   title         = "Star Wars"
   description   = "Visualize Star Wars character, species, planet, spaceship data, and more using Powerpipe and DuckDB."
   color         = "#FFE81F"
-  documentation = file("./index.md")
+  documentation = file("./docs/index.md")
   icon          = "/images/mods/turbot/starwars-insights.svg"
   categories    = ["dashboard", "duckdb"]
 
