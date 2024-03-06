388cbe6ece45ad75dd2e8c762d6f8edab20547b2
sourav chakraborty <sourav@turbot.com>
2024-03-06 21:39:03 +0530

    use insights for icon and image

--- mod.pp
+++ mod.pp
@@ -3,12 +3,12 @@ mod "starwars" {
   description   = "Visualize Star Wars character, species, planet, spaceship data, and more using Powerpipe and DuckDB."
   color         = "#FFE81F"
   documentation = file("./index.md")
-  icon          = "/images/mods/turbot/starwars-dashboard.svg"
+  icon          = "/images/mods/turbot/starwars-insights.svg"
   categories    = ["dashboard", "duckdb"]
 
   opengraph {
     title       = "Powerpipe Mod for Star Wars"
     description = "Visualize Star Wars character, species, planet, spaceship data, and more using Powerpipe and DuckDB."
-    image       = "/images/mods/turbot/starwars-dashboard-social-graphic.png"
+    image       = "/images/mods/turbot/starwars-insights-social-graphic.png"
   }
 }
